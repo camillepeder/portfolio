7a39ecd9169ba4bc02684d6ad3caf996addc37ce
gonzalopedernera <gonzalopeder2@gmail.com>
2024-03-26 20:48:12 +0000

    Modified ToDo project

---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,38 @@
+import Header from './Header'
+import HeroSection from './HeroSection'
+import ProjectCard from './ProjectCard'
+import IrisIcon from '../assets/Iris.svg'
+import ToDoIcon from '../assets/ToDo.png'
+
+function Wrapper() {
+  let Iris = `
+  Iris is my first experience as a front-end developer and UX/UI designer, being this the final project of my journey through Holberton School.
+  By including Iris here, i want to showcase my first approach on web design.
+  `;
+
+  let ToDo = `
+  This is a ToDo project made fully with React. With it, you can create, visualize, edit and delete tasks, displayed on a list.
+  This project is a little demonstrartion of my habilities as a front-end developer.
+  `;
+
+
+  return (
+    <>
+    <Header />
+    <div className='justify-center items-center flex flex-col gap-5 py-5'>
+      {/* Hero */}
+
+        <HeroSection />
+      <div className='m-5'>
+        <h1 className='text-4xl'> My Projects</h1>
+      </div>
+      <div className='flex flex-row gap-10 justify-center items-center'>
+        <ProjectCard name='Iris' description={Iris} img={IrisIcon} link='https://youtu.be/aFq4CJf55Ek'/>
+        <ProjectCard name='ToDo' description={ToDo} img={ToDoIcon} link='https://github.com/gonzalopedernera/ToDo'/>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Wrapper
