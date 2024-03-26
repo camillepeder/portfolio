72febfc17a5f9a65a59f8220e2e233fe7f2c219c
gonzalopedernera <gonzalopeder2@gmail.com>
2024-03-26 20:44:32 +0000

    Some visual tweaks and function modifications

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,5 @@
 
-
-export default function ProjectCard({ name, description, longDescription, img, link }) {
+export default function ProjectCard({ name, description, img, link }) {
 
   const btnRedirect = () => {
     window.open(link, '_blank')
@@ -22,11 +21,11 @@ export default function ProjectCard({ name, description, longDescription, img, l
         </div>
         {/* Hover div */}
         <div className=' cardDescription w-full h-full top-0 absolute bg-black rounded-2xl backdrop-blur-md -right-full p-2'>
-          <div className='text-xl text-white p-2 flex flex-col gap-10 justify-between items-center'>
-            <div className='flex flex-col justify-between text-left px-3 py-1 gap-7'>
-              <div className="flex flex-col justify-center items-center">
+          <div className='text-xl text-white p-2 flex flex-col gap-10 justify-center items-center'>
+            <div className='flex flex-col justify-between text-left px-3 py-1 gap-7 card-text'>
+              <div className="flex flex-col justify-center items-center top-0 h-full">
                 <h1 className='text-4xl uppercase'>{name}:</h1>
-                <p className=' whitespace-pre-line break-words'>{longDescription}</p>
+                <p className=' whitespace-pre-line break-words'>{description}</p>
               </div>
               <button className="btn btn-neutral w-2/5 customFont" onClick={btnRedirect}>Go to project</button>
             </div>
